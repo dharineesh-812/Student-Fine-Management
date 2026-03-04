bbc858ad8df754f72ae0d6399d27ca28fcc6ae8f
DHARINEESH N <dharineesh812@gmail.com>
2026-03-04 15:34:41 +0530

    Users.java

--- Users.java
+++ Users.java
@@ -5,30 +5,29 @@ public class Users {
     private String name;
     private String pass;
     private String role;
-
     public Users(String email, String name, String pass, String role) {
-        this.email = email.toLowerCase().trim();
-        this.name = name;
-        this.pass = pass.trim();
-        this.role = role.toUpperCase().trim();
+        this.email=email.toLowerCase().trim();
+        this.name=name;
+        this.pass=pass.trim();
+        this.role=role.toUpperCase().trim();
     }
-
     public String getEmail(){ 
     	return email;
-    	}
+    }
     public String getName(){
     	return name; 
     	}
     public String getPass(){ 
     	return pass;
-    	}
+    }
     public String getRole(){
     	return role; 
-    	}
+    }
     public void setName(String name){ 
     	this.name = name;
     	}
     public void setPass(String pass){ 
     	this.pass = pass;
     	}
+
 }
